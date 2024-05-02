817191e4ada753656f90539481178633a21b7055
mawlm5007 <maahirmawla24@gmail.com>
2024-05-02 17:48:42 -0400

    updating rakaat count based on proximity

--- App.tsx
+++ App.tsx
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -21,44 +13,13 @@ import type { SubscriptionRef } from 'rn-proximity-sensor';
 
 import {
   Colors,
-  DebugInstructions,
   Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
   const [isProximityEnabled, setProximityEnabled] = React.useState<boolean>(false);
+  const [proximityCount, setProximityCount] = React.useState<number>(0);
+
   const sensorSubscriptionRef = React.useRef<SubscriptionRef | null>(null);
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -68,8 +29,20 @@ function App(): React.JSX.Element {
 
   React.useEffect(() => {
     sensorSubscriptionRef.current = proximity.subscribe((values) => {
-      if (values.is_double_toggle) setProximityEnabled((prev) => !prev);
-    });
+      if (values.is_double_toggle) {
+        setProximityEnabled(true);
+        setProximityCount((prevCount) => {
+          const updatedCount = prevCount + 1;
+          if (updatedCount % 2 === 0 && updatedCount !== 0) {
+            console.log('One rakaat done!'); 
+            return 0;
+          }
+          return updatedCount;
+        });
+      } else {
+        setProximityEnabled(false);
+      }
+    }); 
 
     return () => {
       if (sensorSubscriptionRef.current) {
@@ -93,30 +66,11 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Text>Open up App.tsx to start working on your app! {isProximityEnabled}</Text>
+          <Text>Open up App.tsx to start working on your app! isProximityEnabled: { isProximityEnabled ? 'on' : 'off'} proximityCount: {proximityCount}</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
