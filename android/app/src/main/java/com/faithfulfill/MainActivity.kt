8571fe4cdc5844ad8e7ef6d312bf8b8cb16c5bb2
mawlm5007 <maahirmawla24@gmail.com>
2024-05-10 22:13:24 -0400

    storyboard

--- android/app/src/main/java/com/faithfulfill/MainActivity.kt
+++ android/app/src/main/java/com/faithfulfill/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.faithfulfill
 
+import android.os.Bundle; // Add this import statement
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,11 +14,16 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "FaithFulfill"
-
+  
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  // Add the highlighted code to the body of MainActivity class
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
